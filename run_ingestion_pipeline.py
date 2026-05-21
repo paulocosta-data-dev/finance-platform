26f93ed6a7fbd816c4d1005ada8217d9064b7dcf
paulocosta-data-dev <pdc.geral@gmail.com>
2026-05-21 08:00:11 +0100

    pipeline add runtime replay and rebuild controls

--- run_ingestion_pipeline.py
+++ run_ingestion_pipeline.py
@@ -1,9 +1,29 @@
+import argparse
+
 from app.pipelines.ingestion_pipeline import (
     run_ingestion_pipeline,
 )
 
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--force-reprocess",
+    action="store_true",
+)
+
+parser.add_argument(
+    "--rebuild-bronze",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
 run_ingestion_pipeline(
-    force_reprocess=False,
-    rebuild_bronze=False,
+    force_reprocess=(
+        args.force_reprocess
+    ),
+    rebuild_bronze=(
+        args.rebuild_bronze
+    ),
 )
--- run_normalization_pipeline.py
+++ run_normalization_pipeline.py
@@ -1,8 +1,21 @@
+import argparse
+
 from app.pipelines.normalization_pipeline import (
     run_normalization_pipeline,
 )
 
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--rebuild-silver",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
 run_normalization_pipeline(
-    rebuild_silver=False
+    rebuild_silver=(
+        args.rebuild_silver
+    ),
 )
